fe95742e4b3c5b3bdf4fb36288f72756207700ce
Behzad Khamneli <bkhamneli6@gmail.com>
2021-09-19 16:49:10 -0400

    Update config.js

--- gnufx-server/config.js
+++ gnufx-server/config.js
@@ -6,11 +6,11 @@ var os = require("os");
 
 const config = {
     secretKey: "secretKey",
-    database: `mongodb+srv://behdad:ToeFXDev2021@user.nurmz.mongodb.net/user?retryWrites=true&w=majority`,
+    database: `mongodb+srv://behdad:FXDev2021:D@user.nurmz.mongodb.net/user?retryWrites=true&w=majority`,
     hostType: os.type(),
     dev_client : "http://localhost:3000",
-    dev_email: "toefxdevteam@gmail.com",
-    dev_epass: "Toefxdevteam123"
+    dev_email: "email",
+    dev_epass: "password"
 }
 
 module.exports = config;
